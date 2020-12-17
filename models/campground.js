44bdf66f7fd80eb01bde2fa25797e585400bd45e
Andrew Sanchez <a.vincent.sanchez@gmail.com>
2020-12-17 18:31:05 +0000

    change geometry required

--- models/campground.js
+++ models/campground.js
@@ -20,7 +20,7 @@ const CampgroundSchema = new Schema(
       type: {
         type: String, // Don't do `{ location: { type: String } }`
         enum: ["Point"], // 'location.type' must be 'Point'
-        required: true,
+        // required: true,
       },
       coordinates: {
         type: [Number],
